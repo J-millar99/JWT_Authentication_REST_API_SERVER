b900ae1a5005055c0c97baf0b1d8b8f7cc31ddd2
J-millar99 <wlwogus33@gmail.com>
2026-04-09 15:54:05 +0900

    Project Init

---
+++ src/main/java/com/example/JWT_Authentication_REST_API_SERVER/JwtAuthenticationRestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_Authentication_REST_API_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationRestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationRestApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JWT_Authentication_REST_API_SERVER/JwtAuthenticationRestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWT_Authentication_REST_API_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationRestApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
