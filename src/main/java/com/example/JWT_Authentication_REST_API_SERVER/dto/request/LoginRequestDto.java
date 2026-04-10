c8db66c85262e4ec097ccdb3b4544498e3381450
J-millar99 <wlwogus33@gmail.com>
2026-04-10 15:16:01 +0900

    feature: Dto 추가

---
+++ src/main/java/com/example/JWT_Authentication_REST_API_SERVER/dto/request/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.JWT_Authentication_REST_API_SERVER.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+    @NotBlank(message = "아이디는 필수입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
