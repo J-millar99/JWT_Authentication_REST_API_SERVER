f0795d1c9b6923a0c3375a81d5b31aba8bca95ed
J-millar99 <wlwogus33@gmail.com>
2026-04-10 17:52:48 +0900

    feature: JpaAuditing을 추가하여 객체 생성 시간을 추적

--- src/main/java/com/example/JWT_Authentication_REST_API_SERVER/JwtAuthenticationRestApiServerApplication.java
+++ src/main/java/com/example/JWT_Authentication_REST_API_SERVER/JwtAuthenticationRestApiServerApplication.java
@@ -2,7 +2,9 @@ package com.example.JWT_Authentication_REST_API_SERVER;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JwtAuthenticationRestApiServerApplication {
 
